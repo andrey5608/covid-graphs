9cd887aa0fcb41bd769dc05076038e1d1910ee2d
Andrey Petrov <andrey5608@gmail.com>
2020-04-05 22:53:39 +0300

    Updates

---
+++ precache-manifest.9826735b90ad5fff87ec243ec311fe1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5214e75309edc8e3eab69122718ae930",
+    "url": "/covid-graphs/index.html"
+  },
+  {
+    "revision": "52e324cd5e6715b1e927",
+    "url": "/covid-graphs/static/css/main.a29208db.chunk.css"
+  },
+  {
+    "revision": "b67859e38fc077dcf06c",
+    "url": "/covid-graphs/static/js/2.5eb77c5c.chunk.js"
+  },
+  {
+    "revision": "8d26b2e3700774aaa3e681d097f59854",
+    "url": "/covid-graphs/static/js/2.5eb77c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e324cd5e6715b1e927",
+    "url": "/covid-graphs/static/js/main.b7296e88.chunk.js"
+  },
+  {
+    "revision": "bb0a2a39cfdf8daf932b",
+    "url": "/covid-graphs/static/js/runtime-main.1e615967.js"
+  }
+]);
