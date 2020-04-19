da9da8d9da70039f30ca371571611069369f5d2b
Andrey Petrov <andrey5608@gmail.com>
2020-04-19 21:40:17 +0300

    fix mobile version layout bug

---
+++ build/precache-manifest.a6571f1c0a87fa917bf940559836dd20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0d875c6fd0f41462c8d822a831db54",
+    "url": "/covid-graphs/index.html"
+  },
+  {
+    "revision": "c9a7d07815524f755402",
+    "url": "/covid-graphs/static/css/main.5557bda8.chunk.css"
+  },
+  {
+    "revision": "a7cbc02b6438ee5a2173",
+    "url": "/covid-graphs/static/js/2.f34d9e68.chunk.js"
+  },
+  {
+    "revision": "4a6a2e64d793e9f3261bae43c6a4fdc4",
+    "url": "/covid-graphs/static/js/2.f34d9e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9a7d07815524f755402",
+    "url": "/covid-graphs/static/js/main.1f840297.chunk.js"
+  },
+  {
+    "revision": "bb0a2a39cfdf8daf932b",
+    "url": "/covid-graphs/static/js/runtime-main.1e615967.js"
+  }
+]);
