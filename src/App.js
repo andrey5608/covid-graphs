dbd16c4632050120299d9f160840c28d83aaac28
Andrey Petrov <andrey5608@gmail.com>
2020-05-05 22:19:21 +0300

    Added russian graph by default

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dataset from "@bit/nexxtway.react-rainbow.dataset";
 import moment from "moment";
 import { Dropdown } from "@bit/primefaces.primereact.dropdown";
 import PrimereactStyle from "@bit/primefaces.primereact.internal.stylelinks";
+import { Grid } from "@bit/bit.base-ui.layout.grid-component";
 import "./App.css";
 
 const apiEnpoint = `https://andrey5608.github.io/covid19/timeseries.json`;
@@ -17,8 +18,12 @@ export default class App extends Component {
       cases: undefined,
       deaths: undefined,
       recovered: undefined,
-      country: "Russia",
+      country: "US",
       countries: undefined,
+      russiaDates: undefined,
+      russiaCases: undefined,
+      russiaDeaths: undefined,
+      russiaRecovered: undefined,
     };
 
     this.handlePickUp = this.handlePickUp.bind(this);
@@ -57,16 +62,29 @@ export default class App extends Component {
       alert(`The country '${country}' was unexpected`);
     }
 
-    this.setState({
-      dates: dates,
-      cases: cases,
-      deaths: deaths,
-      recovered: recovered,
-    });
+    switch (country.toLowerCase()) {
+      case "russia":
+        this.setState({
+          russiaDates: dates,
+          russiaCases: cases,
+          russiaDeaths: deaths,
+          russiaRecovered: recovered,
+        });
+        break;
+      default:
+        this.setState({
+          dates: dates,
+          cases: cases,
+          deaths: deaths,
+          recovered: recovered,
+        });
+        break;
+    }
   }
 
   async componentDidMount() {
-    await this.refreshDataForCountry(this.state.country);
+    this.refreshDataForCountry("Russia");
+    this.refreshDataForCountry("US");
   }
 
   async handlePickUp(value) {
@@ -93,46 +111,78 @@ export default class App extends Component {
           </a>
         </header>
         <div className="App-main">
-          <div style={containerStyles}>
-            <div>
-              <Chart
-                labels={this.state.dates}
-                type="line"
-                backgroundColor="white"
-              >
-                <Dataset
-                  title="Deaths"
-                  values={this.state.deaths}
-                  backgroundColor="red"
-                  borderColor="red"
-                />
-                <Dataset
-                  title="Total cases"
-                  values={this.state.cases}
-                  backgroundColor="whitesmoke"
-                  borderColor="whitesmoke"
-                />
-                <Dataset
-                  title="Recovered"
-                  values={this.state.recovered}
-                  backgroundColor="green"
-                  borderColor="green"
-                />
-              </Chart>
+          <Grid col={1} colSm={1}>
+            <div style={containerStyles}>
+              <h4>Russia</h4>
+              <div>
+                <Chart
+                  labels={this.state.russiaDates}
+                  type="line"
+                  backgroundColor="white"
+                >
+                  <Dataset
+                    title="Deaths"
+                    values={this.state.russiaDeaths}
+                    backgroundColor="red"
+                    borderColor="red"
+                  />
+                  <Dataset
+                    title="Total cases"
+                    values={this.state.russiaCases}
+                    backgroundColor="whitesmoke"
+                    borderColor="whitesmoke"
+                  />
+                  <Dataset
+                    title="Recovered"
+                    values={this.state.russiaRecovered}
+                    backgroundColor="green"
+                    borderColor="green"
+                  />
+                </Chart>
+              </div>
             </div>
-            <div>
-              <Dropdown
-                style={{ width: 150 }}
-                value={this.state.country}
-                options={this.state.countries}
-                onChange={(e) => {
-                  this.setState({ country: e.value });
-                  this.handlePickUp(e.value);
-                }}
-                placeholder="Select a country"
-              />
+            <div style={containerStyles}>
+              <h4>Others</h4>
+              <div>
+                <Chart
+                  labels={this.state.dates}
+                  type="line"
+                  backgroundColor="white"
+                >
+                  <Dataset
+                    title="Deaths"
+                    values={this.state.deaths}
+                    backgroundColor="red"
+                    borderColor="red"
+                  />
+                  <Dataset
+                    title="Total cases"
+                    values={this.state.cases}
+                    backgroundColor="whitesmoke"
+                    borderColor="whitesmoke"
+                  />
+                  <Dataset
+                    title="Recovered"
+                    values={this.state.recovered}
+                    backgroundColor="green"
+                    borderColor="green"
+                  />
+                </Chart>
+              </div>
+              <div>
+                <Dropdown
+                  style={{ width: 150 }}
+                  value={this.state.country}
+                  options={this.state.countries}
+                  onChange={(e) => {
+                    this.setState({ country: e.value });
+                    this.handlePickUp(e.value);
+                  }}
+                  placeholder="Select a country"
+                />
+              </div>
             </div>
-          </div>
+          </Grid>
         </div>
       </div>
     );
